3ae7fd860b78375e184ac0d6006511dc6b52289f
jacds <bclarinetman@gmail.com>
2016-12-14 18:15:46 +0000

    Added needed lines of code

--- src/AlbumPage.java
+++ src/AlbumPage.java
@@ -1,9 +1,11 @@
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.ArrayList;
 
 /**
  * Created by Rocha on 14/12/2016.
@@ -15,8 +17,17 @@ public class AlbumPage extends HttpServlet {
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
         String name = request.getParameter("name").replace("+"," ");
+
+        QueryManager qm = new QueryManager();
+        ArrayList<ArrayList<String>> result = qm.getAlbumInfo(name);
+        ArrayList<String> albumInfo = result.get(0);
+        //ArrayList<String> albumTracks = result.get(1);
+
+        request.setAttribute("result", albumInfo);
+        //request.setAttribute("tracks", albumTracks);
+        RequestDispatcher view=request.getRequestDispatcher("albumPage.jsp");
+        view.forward(request,response);
 
     }
 }
